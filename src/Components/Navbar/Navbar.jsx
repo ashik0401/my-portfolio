ff918d9341d0958858776403e5e48b70fecae869
ashik0401 <ashik04am@gmail.com>
2025-06-30 00:39:13 +0600

    added logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -68,36 +68,43 @@ const Navbar = () => {
     return (
         <div className="fixed top-0 left-0 w-full z-100  backdrop-blur shadow-md">
             <div className="navbar text-white md:w-10/12 mx-auto">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-[#2C2F48] rounded-box z-1 mt-3 w-30 p-2 shadow">
-                            {link}
-                        </ul>
-                    </div>
+                <div
+                    onClick={() => scroll.scrollToTop()}
+                    className="navbar-start">
+
                     <a
-                        onClick={() => scroll.scrollToTop()}
+
                         className="text-xl text-orange-300 cursor-pointer"
                     >
                         <img
                             className='max-w-15 max-h-15'
                             src={logo} alt="" />
+
                     </a>
+                    <h2 className='text-xl font-semibold text-orange-400'>Ashik Mahmud</h2>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1 text-xs">{link}</ul>
                 </div>
                 <div className="navbar-end">
-                    <motion.button
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden ">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <div className='-ml-20'>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-[#2C2F48] rounded-box z-1 mt-3 w-30 p-2 shadow">
+                                {link}
+                            </ul>
+                        </div>
+                    </div>
+                    {/* <motion.button
                         className=" bg-orange-500 hover:bg-orange-600 text-white font-semibold py-3 px-6 rounded-full shadow-lg transition duration-300"
                         whileHover={{ scale: 1.05 }}
                     >
                         Resume
-                    </motion.button>
+                    </motion.button> */}
                 </div>
             </div>
         </div>
